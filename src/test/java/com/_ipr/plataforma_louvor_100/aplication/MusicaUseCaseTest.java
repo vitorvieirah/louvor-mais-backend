218c329e2d78b441b19558fb7a7ae779f327e7bd
VitorVieira <vitorhvvieira@gmail.com>
2025-03-23 10:32:34 -0300

    Fazendo teste do método de cadastro de musica do usecase

--- src/test/java/com/_ipr/plataforma_louvor_100/aplication/MusicaUseCaseTest.java
+++ src/test/java/com/_ipr/plataforma_louvor_100/aplication/MusicaUseCaseTest.java
@@ -1,14 +1,17 @@
 package com._ipr.plataforma_louvor_100.aplication;
 
+import com._ipr.plataforma_louvor_100.aplication.gateways.MusicaGateway;
+import com._ipr.plataforma_louvor_100.builder.MusicaBuilder;
 import com._ipr.plataforma_louvor_100.domain.musica.Musica;
 import com._ipr.plataforma_louvor_100.infrastructure.dataprovider.MusicaDataProvider;
+import com._ipr.plataforma_louvor_100.validators.MusicaValidator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class MusicaUseCaseTest {
 
     @Mock
-    private MusicaDataProvider dataProvider;
+    private MusicaGateway gateway;
 
     @InjectMocks
     private MusicaUseCase useCase;
@@ -24,11 +27,24 @@ class MusicaUseCaseTest {
     @Captor
     private ArgumentCaptor<Musica> captor;
 
-    private
+    private final Musica musicaTeste = MusicaBuilder.gerarMusicaDomain();
 
     @Test
     void testeCadastroMusica() {
 
+        Mockito.when(gateway.consultarPorId(Mockito.any())).thenReturn(Optional.empty());
+
+        Mockito.when(gateway.salvar(captor.capture())).thenReturn(musicaTeste);
+
+        musicaTeste.setIdMusica(null);
+
+        useCase.cadastrar(musicaTeste);
+
+        Musica resultado = captor.getValue();
+
+
+        MusicaValidator.validaMusicaDomain(resultado, musicaTeste);
+        Assertions.assertNull(resultado.getIdMusica());
     }
 
     @Test
